a89edddf744240a9ba318ca32f539131594c6216
2019211001000801HeYuxin <415097064@qq.com>
2021-03-27 15:17:22 +0800

    4.HW-CE1 Week4 Commit

---
+++ src/main/java/com/HeYuxin/week4/homework/ConfigDemoServlet.java
@@ -0,0 +1,34 @@
+package com.HeYuxin.week4.homework;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name = "name",value = "He Yuxin"),
+                @WebInitParam(name = "studentId",value = "2019211001000801")
+        },
+        loadOnStartup = 1
+)
+
+public class ConfigDemoServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = getServletConfig().getInitParameter("name");
+        String studentId = getServletConfig().getInitParameter("studentId");
+
+        PrintWriter out = response.getWriter();
+        out.println("<br/>name:"+name);
+        out.println("<br/>studentId:"+studentId);
+    }
+}
