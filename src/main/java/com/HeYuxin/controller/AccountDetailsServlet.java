8d5e2e62a920dead3ae7b5838492130ec8340e2c
2019211001000801HeYuxin <415097064@qq.com>
2021-06-07 19:40:11 +0800

    14.HW-CE Week14 Commit

--- src/main/java/com/HeYuxin/controller/AccountDetailsServlet.java
+++ src/main/java/com/HeYuxin/controller/AccountDetailsServlet.java
@@ -20,35 +20,38 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
 
-@WebServlet("/accountDetails")
+@WebServlet(name = "AccountDetailsServlet", value = "/accountDetails")
 public class AccountDetailsServlet extends HttpServlet {
     Connection con=null;
+    @Override
     public void init() throws ServletException {
-        con=(Connection)getServletContext().getAttribute("con");
-    }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        super.init();
+        con=(Connection) getServletContext().getAttribute("con");
     }
 
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session=request.getSession(false);
-        if(session!=null&& session.getAttribute("user")!=null) {
-            User user=(User)session.getAttribute("user");
-            int id=user.getId();
-            UserDao dao=new UserDao();
-            try{
-                user=dao.findById(con,id);
-                request.setAttribute("user",user);
-                OrderDao orderDao=new OrderDao();
-                List<Order> orderList=orderDao.findByUserId(con,id);
-                request.setAttribute("orderList",orderList);
-                request.getRequestDispatcher("WEB-INF/views/accountDetails.jsp").forward(request,response);
-            }catch (Exception e) {
-                System.out.println(e);
-            }
-        }
-        else{
+        HttpSession session = request.getSession(false);
+        if(session!=null && session.getAttribute("user")!=null){
+            User user=(User) session.getAttribute("user");
+            int userId= user.getId();
+            request.setAttribute("user",user);
+            OrderDao orderDao=new OrderDao();
+            List<Order> orderList=orderDao.findByUserId(con,userId);
+            request.setAttribute("orderList",orderList);
+            String path="/WEB-INF/views/accountDetails.jsp";
+            request.getRequestDispatcher(path).forward(request,response);
+        }else{
             response.sendRedirect("login");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
     }
 }
