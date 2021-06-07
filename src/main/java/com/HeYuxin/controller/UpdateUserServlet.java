6c0df8b47bcace4af85ebd3f7f645f9225e3c002
2019211001000801HeYuxin <415097064@qq.com>
2021-06-07 19:34:05 +0800

    14.HW-CE Week14 Commit

--- src/main/java/com/HeYuxin/controller/UpdateUserServlet.java
+++ src/main/java/com/HeYuxin/controller/UpdateUserServlet.java
@@ -85,7 +85,7 @@ public class UpdateUserServlet extends HttpServlet {
                     HttpSession session = request.getSession();
                     session.setMaxInactiveInterval(10);
                     session.setAttribute("user", u);
-                    request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request, response);
+                    request.getRequestDispatcher("accountDetails").forward(request, response);
                 }
             } catch (SQLException throwable) {
                 throwable.printStackTrace();
